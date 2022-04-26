d66e882c8e99df3a56357cab9e9b79d8c58d08a1
3li-3bdullah <PROGRMMER7@gmail.com>
2022-04-27 00:10:49 +0200

    I've imported the ScreenPainter path

--- tic_tac_toe/lib/view/tic_tac_toe.dart
+++ tic_tac_toe/lib/view/tic_tac_toe.dart
@@ -1,22 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:tic_tac_toe/view/tic_tac_toe_widgets/screen_painter.dart';
 
 class TicTacToe extends StatelessWidget {
-  const TicTacToe({ Key? key }) : super(key: key);
+  const TicTacToe({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Tic Tac Toe" , style: TextStyle(color: Colors.black),),
+        title: const Text(
+          "Tic Tac Toe",
+          style: TextStyle(color: Colors.black),
+        ),
         centerTitle: true,
         backgroundColor: Colors.white,
       ),
       body: Center(
         child: AspectRatio(
           aspectRatio: 1.0,
-          child: Container(child: 
-          CustomPaint(painter: ScreenPainter(),)),
-          ),
+          child: Container(
+              child: CustomPaint(
+            painter: ScreenPainter(),
+          )),
+        ),
       ),
     );
   }
