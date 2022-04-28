31f51d0e9a594acde5b446f8183a31ef65920668
3li-3bdullah <PROGRMMER7@gmail.com>
2022-04-28 11:50:12 +0200

    I've converted the class to statefulwidget

--- tic_tac_toe/lib/view/tic_tac_toe.dart
+++ tic_tac_toe/lib/view/tic_tac_toe.dart
@@ -1,8 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:tic_tac_toe/view/tic_tac_toe_widgets/screen_painter.dart';
+import 'package:tic_tac_toe/constants.dart';
 
-class TicTacToe extends StatelessWidget {
-  const TicTacToe({Key? key}) : super(key: key);
+
+class TicTacToe extends StatefulWidget {
+   const TicTacToe({Key? key}) : super(key: key);
+
+  @override
+  State<TicTacToe> createState() => _TicTacToeState();
+}
+
+class _TicTacToeState extends State<TicTacToe> {
+  Mark currentMark = Mark.O;
+
+  Map<int,Mark> gameMarks = {};
+
+  void addMark(double x , double y){
+   double divideSize = ScreenPainter.getDivideSize();
+   bool isAbsent = false;
+   gameMarks.putIfAbsent(
+    (x ~/ divideSize + (y ~/ divideSize) * 3).toInt(),
+    () {
+      isAbsent = true ; return currentMark;
+   }
+   );
+   if(isAbsent) currentMark = currentMark == Mark.O ? Mark.X : Mark.O;
+ }
 
   @override
   Widget build(BuildContext context) {
@@ -10,20 +32,31 @@ class TicTacToe extends StatelessWidget {
       appBar: AppBar(
         title: const Text(
           "Tic Tac Toe",
-          style: TextStyle(color: Colors.black),
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
         ),
         centerTitle: true,
-        backgroundColor: Colors.white,
+        elevation: 0,
+        backgroundColor: Colors.pink[500],
       ),
       body: Center(
-        child: AspectRatio(
-          aspectRatio: 1.0,
-          child: Container(
-              child: CustomPaint(
-            painter: ScreenPainter(),
-          )),
-        ),
-      ),
+          child: GestureDetector(
+                onTapUp: (TapUpDetails details) {
+                  if (gameMarks.length >= 9 ) {
+                    gameMarks = <int, Mark>{};
+                    currentMark = Mark.O;
+                  } else {
+                   addMark(details.localPosition.dx, details.localPosition.dy);
+                  }
+                },
+                child: AspectRatio(
+                  aspectRatio: 1.0,
+                  child: Container(
+                      child: CustomPaint(
+                    painter: ScreenPainter(gameMarks: gameMarks),
+                  )),
+                ),
+              )
+              ),
     );
   }
 }
