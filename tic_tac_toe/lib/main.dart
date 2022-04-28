9175f734379a726b2564edcf7638074e07516e4c
3li-3bdullah <PROGRMMER7@gmail.com>
2022-04-28 21:59:11 +0200

    I've changed the color

--- tic_tac_toe/lib/main.dart
+++ tic_tac_toe/lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       title: 'Tic Tac Toe',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        scaffoldBackgroundColor: Colors.pink[600],
+        scaffoldBackgroundColor: Colors.black,
       ),
       home:  const TicTacToe()
     );
