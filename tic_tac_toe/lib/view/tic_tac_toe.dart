f5c1eb35f79237e47470823446aa8f68e47cbe1a
3li-3bdullah <PROGRMMER7@gmail.com>
2022-04-28 11:52:42 +0200

    I pasted the class that i have typed in file tic_tac_toe_widgets

--- tic_tac_toe/lib/view/tic_tac_toe.dart
+++ tic_tac_toe/lib/view/tic_tac_toe.dart
@@ -60,3 +60,83 @@ class _TicTacToeState extends State<TicTacToe> {
     );
   }
 }
+
+class ScreenPainter extends CustomPainter {
+  static double? divideSize;
+  Map<int, Mark>? gameMarks;
+  ScreenPainter({required this.gameMarks});
+  @override
+  void paint(Canvas canvas, Size size) {
+    divideSize = size.width / 3;
+    final customPaint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = stroke_width
+      ..color = Colors.white;
+    final whiteThickPaint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = double_stroke_width
+      ..color = Colors.white;
+    final yellowThickPaint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = double_stroke_width
+      ..color = Colors.yellow;
+
+    //First Horizontal Line --
+    canvas.drawLine(Offset(stroke_width, divideSize! - half_stroke_width),
+        Offset(size.width, divideSize! - half_stroke_width), customPaint);
+    //First Vertical Line |
+    canvas.drawLine(Offset(divideSize! - half_stroke_width, stroke_width),
+        Offset(divideSize! - half_stroke_width, size.height), customPaint);
+    //Second Horizontal Line --
+    canvas.drawLine(Offset(stroke_width, divideSize! * 2.0 - half_stroke_width),
+        Offset(size.width, divideSize! * 2.0 - half_stroke_width), customPaint);
+    //Second vertical Line |
+    canvas.drawLine(
+        Offset(divideSize! * 2.0 - half_stroke_width, stroke_width),
+        Offset(divideSize! * 2.0 - half_stroke_width, size.height),
+        customPaint);
+
+    gameMarks!.forEach((index, mark) {
+      switch (mark) {
+        case Mark.O:
+          drawNought(canvas, index, yellowThickPaint);
+          break;
+        case Mark.X:
+          drawCross(canvas, index, whiteThickPaint);
+          break;
+        default:
+          break;
+      }
+    });
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
+
+  static double getDivideSize() => divideSize!;
+
+  // This methods to paint the circular shape
+  void drawNought(Canvas canvas, int index, Paint paint) {
+    double left = (index % 3) * divideSize! + double_stroke_width * 2;
+    double top = (index ~/ 3) * divideSize! + double_stroke_width * 2;
+    double noughtSize = divideSize! - double_stroke_width * 4;
+    canvas.drawOval(Rect.fromLTWH(left, top, noughtSize, noughtSize), paint);
+  }
+
+  // Logic
+  void drawCross(Canvas canvas, int index, Paint paint) {
+    double x1, y1;
+    double x2, y2;
+    x1 = (index % 3) * divideSize! + double_stroke_width * 2;
+    y1 = (index ~/ 3) * divideSize! + double_stroke_width * 2;
+    x2 = (index % 3 + 1) * divideSize! - double_stroke_width * 2;
+    y2 = (index ~/ 3 + 1) * divideSize! - double_stroke_width * 2;
+    canvas.drawLine(Offset(x1, y1), Offset(x2, y2), paint);
+
+    x1 = (index % 3 + 1) * divideSize! - double_stroke_width * 2;
+    y1 = (index ~/ 3) * divideSize! + double_stroke_width * 2;
+    x2 = (index % 3) * divideSize! + double_stroke_width * 2;
+    y2 = (index ~/ 3 + 1) * divideSize! - double_stroke_width * 2;
+    canvas.drawLine(Offset(x1, y1), Offset(x2, y2), paint);
+  }
+}
