6b7fd4187ac27caa4ce06a15bc9a9e561117a4f0
3li-3bdullah <PROGRMMER7@gmail.com>
2022-04-28 22:16:34 +0200

    I've removed the getMaterial

--- tic_tac_toe/lib/main.dart
+++ tic_tac_toe/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:tic_tac_toe/view/tic_tac_toe.dart';
 
 void main() {
@@ -12,7 +11,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    return MaterialApp(
       title: 'Tic Tac Toe',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
